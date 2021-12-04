ad3da0a2cd1b39bd4aaedc97f723984fbbf96e3a
Alexander Burachkov <zicio@yandex.ru>
2021-12-04 22:17:33 +0400

    Update determiner.test.js

--- src/js/__tests__/determiner.test.js
+++ src/js/__tests__/determiner.test.js
@@ -10,7 +10,7 @@ test.each([
   ['345582584280276', 'card__amex'],
   ['2200000000000000', 'card__mir'],
   ['3535436455242069', 'card__jcb'],
-  ['12345678', null]
+  ['12345678', null],
 ])(('should be show true system'), (input, expected) => {
   expect(new Determiner(input).check()).toBe(expected);
 });
